6e906c9dbbc20615a83efee8a980bac6afc9860a
Arjun Narve <arjunnarve04@gmail.com>
2025-03-13 16:07:00 +0530

    Track automation changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World this is PES2UG22CS910\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "I made few changes to check the automation of jenkins";
   return 0;
 }
