e3b1bf6bfa1c9b9e70773757a00e7c5c84788bcb
Arjun Narve <arjunnarve04@gmail.com>
2025-03-13 16:15:30 +0530

    Update output message in hello.cpp to enhance clarity

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World this is PES2UG22CS910\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Jenkins is working fine absolutely\n";
+  cout << "Jenkins is working fine absolutely!!!\n";
   return 0;
 }
